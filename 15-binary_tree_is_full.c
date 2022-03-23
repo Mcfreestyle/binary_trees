e605128b30d35e521ce299c6a70506234ef74098
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-03-23 13:10:06 -0500

    feat: checks if a binary tree is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: 1 if the tree is full, otherwise 0 or if @tree is NULL
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left_full, right_full;
+
+	if (!tree)
+		return (0);
+
+	left_full = binary_tree_is_full(tree->left);
+	right_full = binary_tree_is_full(tree->right);
+
+	if (left_full == right_full)
+		return (1);
+	return (0);
+}
